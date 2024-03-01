e9535aa77209a48cb0332276fe1643483afb9237
NadeemAkhter <nadeem.akhter1994@gmail.com>
2024-03-01 21:32:05 +0500

    Worked on login controller and verify user middleware

--- server/router/route.js
+++ server/router/route.js
@@ -7,7 +7,7 @@ const router = Router();
 router.route("/register").post(controller.register);
 // router.route("/registerMail").post();
 router.route("/authenticate").post((req, res) => res.end());
-router.route("/login").post(controller.login);
+router.route("/login").post(controller.verifyUser, controller.login);
 
 //GET Methods
 router.route("/user/:userName").get(controller.getUser);
